6715cc84d1b92f7e25ab030bed5b295133722007
Serhii [boonya] Buinytskyi <779184+boonya@users.noreply.github.com>
2025-05-21 15:35:02 +0200

    fix: CR

--- tests/setup/global.ts
+++ tests/setup/global.ts
@@ -1,5 +1,5 @@
-const global = () => {
+const setupGlobalEnv = () => {
   process.env.TZ = 'Atlantic/Reykjavik'; // This one is quite similar to UTC+0 and does not have DST
 };
 
-export default global;
+export default setupGlobalEnv;
